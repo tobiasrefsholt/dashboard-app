9563d60a83b9022e0f66ad2d8504a8028d0a98f8
Tobias Refsholt <tobias@refsholt.net>
2023-10-17 11:11:27 +0200

    Finsished calendar css (ish)
    Co-authored-by: Lisbeth Jahre Haugnes <Turtlee19996@users.noreply.github.com>
    Co-authored-by: Kevin Trondsetås <ListrikLair@users.noreply.github.com>

--- js/viewCalender.js
+++ js/viewCalender.js
@@ -1,7 +1,30 @@
 function getCalendarHTML() {
     return /* html */`
-        <h2 class="widget-header">Kalenderfeed</h2>
+        <h2 class="widget-header">Uke ${model.inputs.mainPage.calendar.showWeekNr || '53'}</h2>
+        <div class="calender-grid">
+            ${getCalendarTime()}
+            <div class="calendar-event" style="grid-column: 2; grid-row: 33 / 37;">Test</div>
+            <div class="calendar-event" style="grid-column: 3; grid-row: 41 / 47;">Test</div>
+            <div class="calendar-event" style="grid-column: 4; grid-row: 21 / 27;">Test</div>
+            <div class="calendar-event" style="grid-column: 5; grid-row: 73 / 77;">Test</div>
+            <div class="calendar-event" style="grid-column: 7; grid-row: 33 / 37;">Test</div>
+        </div>
     `;
+}
+
+function getCalendarTime() {
+    $html = '';
+    for (let i = 0; i < 24; i++) {
+        let time = '';
+        if (i<10) {
+            time += '0';
+        }
+        time += i;
+        $html += /* html */ `
+            <span class="calendar-time">${time}:00</span>
+        `;
+    }
+    return $html;
 }
 
 function getPopupTaskDetailsHTML() {
